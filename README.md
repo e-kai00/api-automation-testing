54cbe7480317754b0973b7b7469021c0cc21426a
Katerina <yelkaterina@gmail.com>
2024-09-13 17:22:05 +0200

    set up project structure

---
+++ README.md
@@ -0,0 +1,4 @@
+MySQL Workbench
+PyTest
+
+(PyCharm)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+
+setup(name='ssqaapitest',
+    version=1.0,
+    description='Practice API testing',
+    packages=find_packages()
+    )
